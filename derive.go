ff6f16b1f8a5946461524c109afe0b26febbf979
kklash <k_klash@protonmail.com>
2022-12-22 23:14:09 -0800

    feat: hash seed and key creation time with argon2id
    Fixes #18 and #17
    
    Adds the argon2id PBKDF as an intermediate step in
    the key derivation process. This increases the cost
    of brute-force attacks.
    
    By including the key creation time as a salt, we also
    reduce the risk of key collisions.

--- derive.go
+++ derive.go
@@ -7,13 +7,22 @@ import (
 	"io"
 	"time"
 
+	"golang.org/x/crypto/argon2"
 	"golang.org/x/crypto/hkdf"
 
 	"github.com/kklash/mnemonikey/pgp"
 )
 
+const (
+	// The following are the Argon2id paramaters used to derive the root key from the seed.
+	argonTimeFactor   uint32 = 4
+	argonMemoryFactor uint32 = 0x80000 // 512MB
+	argonThreads      uint8  = 2
+	argonKeyLen       uint32 = 32
+)
+
 // keyExpandInfoMaster is the string used as the 'info' parameter to the
-// HDKF-Expand function when generating a master key from a seed.
+// HDKF-Expand function when deriving a PGP master key from a root key.
 const keyExpandInfoMaster = "mnemonikey master key"
 
 // keyExpandInfoSubkey builds the HKDF-Expand function's "info" parameter, used
@@ -26,18 +35,19 @@ func keyExpandInfoSubkey(subkeyType SubkeyType, index uint16) []byte {
 	return info
 }
 
-// hkdfExpand expands the given seed into a key of the given size, scoped under info.
-func hkdfExpand(seedBytes []byte, size int, info []byte) ([]byte, error) {
+// hkdfExpand expands the given root key into a key of the given size, scoped under info.
+func hkdfExpand(rootKey []byte, size int, info []byte) ([]byte, error) {
 	keyOutput := make([]byte, size)
-	keyReader := hkdf.Expand(sha256.New, seedBytes, info)
+	keyReader := hkdf.Expand(sha256.New, rootKey, info)
 	if _, err := io.ReadFull(keyReader, keyOutput); err != nil {
 		return nil, err
 	}
 	return keyOutput, nil
 }
 
-// derivePGPKeySet derives a pgp.KeySet by expanding the given seed using the
-// HMAC-based Key Derivation Function (defined in RFC-5869) with SHA256.
+// derivePGPKeySet derives a pgp.KeySet by hashing the given seed and creation timestamp with Argon2id
+// (defined in RFC-9106), and expanding the resulting root key with the HMAC-based Key Derivation
+// Function (defined in RFC-5869) with SHA256.
 //
 // Sets the key's creation time to the given value. Sets the key's user ID
 // and expiry time using values given in the key options.
@@ -51,9 +61,12 @@ func derivePGPKeySet(seed *Seed, creation time.Time, opts *KeyOptions) (*pgp.Key
 		expiry = time.Unix(creation.Add(opts.TTL).Unix(), 0).UTC()
 	}
 
-	seedBytes := seed.Bytes()
+	rootKeySalt := make([]byte, 4)
+	binary.BigEndian.PutUint32(rootKeySalt, uint32(creation.Unix()))
 
-	masterKeySeed, err := hkdfExpand(seedBytes, 32, []byte(keyExpandInfoMaster))
+	rootKey := argon2.IDKey(seed.Bytes(), rootKeySalt, argonTimeFactor, argonMemoryFactor, argonThreads, argonKeyLen)
+
+	masterKeySeed, err := hkdfExpand(rootKey, 32, []byte(keyExpandInfoMaster))
 	if err != nil {
 		return nil, fmt.Errorf("failed to derive master key from seed: %w", err)
 	}
@@ -72,7 +85,7 @@ func derivePGPKeySet(seed *Seed, creation time.Time, opts *KeyOptions) (*pgp.Key
 
 	if opts.subkeyEnabled(SubkeyTypeEncryption) {
 		encKeyInfo := keyExpandInfoSubkey(SubkeyTypeEncryption, opts.EncryptionSubkeyIndex)
-		encryptionSubkeySeed, err := hkdfExpand(seedBytes, 32, encKeyInfo)
+		encryptionSubkeySeed, err := hkdfExpand(rootKey, 32, encKeyInfo)
 		if err != nil {
 			return nil, fmt.Errorf("failed to derive encryption subkey from seed: %w", err)
 		}
@@ -84,7 +97,7 @@ func derivePGPKeySet(seed *Seed, creation time.Time, opts *KeyOptions) (*pgp.Key
 
 	if opts.subkeyEnabled(SubkeyTypeAuthentication) {
 		authKeyInfo := keyExpandInfoSubkey(SubkeyTypeAuthentication, opts.AuthenticationSubkeyIndex)
-		authenticationSubkeySeed, err := hkdfExpand(seedBytes, 32, authKeyInfo)
+		authenticationSubkeySeed, err := hkdfExpand(rootKey, 32, authKeyInfo)
 		if err != nil {
 			return nil, fmt.Errorf("failed to derive authentication subkey from seed: %w", err)
 		}
@@ -96,7 +109,7 @@ func derivePGPKeySet(seed *Seed, creation time.Time, opts *KeyOptions) (*pgp.Key
 
 	if opts.subkeyEnabled(SubkeyTypeSigning) {
 		sigKeyInfo := keyExpandInfoSubkey(SubkeyTypeSigning, opts.SigningSubkeyIndex)
-		signingSubkeySeed, err := hkdfExpand(seedBytes, 32, sigKeyInfo)
+		signingSubkeySeed, err := hkdfExpand(rootKey, 32, sigKeyInfo)
 		if err != nil {
 			return nil, fmt.Errorf("failed to derive signing subkey from seed: %w", err)
 		}
